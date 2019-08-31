d2c4a0db867c00aa33ab0010e799f7a8f01ab26c
Guilherme Vieira <guilhermevieiracosta25@gmail.com>
2019-08-31 17:46:40 -0300

    Iniciando a árvore de busca binária

---
+++ public/Python/Binarytree.py
@@ -0,0 +1,13 @@
+class Nos:
+    def __init__(self, key, value, left, right):
+        self.key = key
+        self.value= value
+        self.left = left
+        self.right = right
+
+class Tree:
+
+    def __init__(self):
+        self.root = Nos(None, None, None, None)
+        self.root = None
+        
