bc20a64171249dd2d9d5a2aa1297f961d6d72f96
Guilherme Vieira <guilhermevieiracosta25@gmail.com>
2019-08-31 21:40:43 -0300

    classes para inserir folhas

--- public/Python/Binarytree.py
+++ public/Python/Binarytree.py
@@ -10,4 +10,25 @@ class Tree:
     def __init__(self):
         self.root = Nos(None, None, None, None)
         self.root = None
+
+    def inserir(self, v):
+       novo = Nos(v, None, None)
+       if self.root == None:
+          self.root = novo
+       else:
+            atual = self.root
+         while True:
+             anterior = atual
+            if v <= atual.item:
+                atual = atual.left
+                if atual == None:
+                  anterior.esq = novo
+                     return
+
+            else: 
+                atual = atual.right
+                if atual == None:
+                     anterior.right = novo
+                     return
+
         
