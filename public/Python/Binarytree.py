ed890d6519d5bc89bcac1051892c6264c9688f66
Guilherme Vieira <guilhermevieiracosta25@gmail.com>
2019-09-04 23:38:37 -0300

    condição para os sockets amd e intel

--- public/Python/Binarytree.py
+++ public/Python/Binarytree.py
@@ -1,34 +1,60 @@
-class Nos:
-    def __init__(self, key, value, left, right):
-        self.key = key
-        self.value= value
-        self.left = left
-        self.right = right
+socket = ""
+gen
+
+
+
+
+
+class No:
+     
+     def __init__(self, key, amd, intel):
+          self.item = key
+          self.amd = amd
+          self.intel = intel
+          self.ger7 = ger7
+          self.ger8 = ger8
 
 class Tree:
 
-    def __init__(self):
-        self.root = Nos(None, None, None, None)
-        self.root = None
+     def __init__(self):
+          self.root = No(None,None,None)
+          self.root = None
 
-    def inserir(self, v):
-       novo = Nos(v, None, None)
-       if self.root == None:
-          self.root = novo
-       else:
-            atual = self.root
-         while True:
-             anterior = atual
-            if v <= atual.item:
-                atual = atual.left
-                if atual == None:
-                  anterior.esq = novo
-                     return
+     def inserir(self, socket):
+          novo = No(socket,None,None) 
+          if self.root == None:
+               self.root = novo
+          else: 
+               atual = self.root
+               while True:
+                    anterior = atual
+                    
+                    if socket = "am4":
+                         atual = atual.amd
+                         if atual == None:
+                                anterior.amd = novo
+                                return
+                    
+                    else:
+                         atual = atual.intel
+                         if atual == None:
+                                 anterior.intel = novo  
+                                    if gen <= 7:
+                                        anterior.ger7 = novo
+                                        else:
+                                            anterior.ger8 = novo
+                                 return
+                
 
-            else: 
-                atual = atual.right
-                if atual == None:
-                     anterior.right = novo
-                     return
-
-        
+     def buscar(self, chave):
+         if self.root == None:
+              return None 
+         atual = self.root 
+         while atual.item != chave: 
+               if chave < atual.item:
+                    atual = atual.amd 
+               else:
+                    atual = atual.intel 
+               if atual == None:
+                    return None 
+         return atual    
