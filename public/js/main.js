24cbe1f4a3d2c867588bf242309bf7180473aaee
Suporte 13 <suporte13@crescer.net>
2019-09-11 15:32:08 -0300

    criando filtro responsivo

--- public/js/main.js
+++ public/js/main.js
@@ -26,4 +26,78 @@ btnPesquisa.click(event => {
 
 
 
+function addIconClasses() {
+    $("#Conteudo-promocoes").each(function(){
+        const scre = $("body").width();
+        const prom = $("#Conteudo-promocoes");
+		const divProm = $("#div-prom");
+        if ( scre <= 991 ) {
+            prom.addClass("container-fluid");
+            prom.removeClass("container");
+            divProm.addClass('col-md-12');
+            divProm.removeClass('col-md-9');
+        } if ( scre > 991 ) {
+            prom.addClass('container');
+            prom.removeClass('container-fluid');
+            divProm.addClass('col-md-9');
+            divProm.removeClass('col-md-12');
+        } 
+
+        if (prom.hasClass('container-fluid')){
+        		const btnIcone = $("#icone-responsive");
+				btnIcone.click(event => {
+				const iToggle = $(event.target);
+				const isToggled = iToggle.attr('data-toggle');
+				const divProm = $("#div-prom");
+
+				if (isToggled === "false") {
+					divProm.removeClass('col-sm-12');
+					divProm.addClass('col-sm-9');
+					iToggle.attr('data-toggle', 'true');
+					divProm.addClass('col-md-9');
+					divProm.removeClass('col-md-12');
+					$(".hidden").css("background-color", 'white');
+					$(".icone").css("color", 'black');
+
+				} else {
+					divProm.removeClass('col-sm-9');
+					divProm.addClass('col-sm-12');
+					iToggle.attr('data-toggle', 'false');
+					divProm.addClass('col-md-12');
+					divProm.removeClass('col-md-9');
+					$(".hidden").css("background-color", 'transparent');
+					$(".icone").css("color", 'white');
+				}
+
+			});
+        }
+    });
+
+}
+
+
+$(document).ready(function () {
+    addIconClasses();
+    $(window).resize(function() {
+        addIconClasses();
+    });
+});
+
+/*const btnIcone = $("#icone-responsive");
+	btnIcone.click(event => {
+	const iToggle = $(event.target);
+	const isToggled = iToggle.attr('data-toggle');
+	const divProm = $("#div-prom");
+	const scre = $("body").width();
+
+	if (isToggled === "false") && (scre > 991) {
+		divProm.removeClass('col-sm-12');
+		divProm.addClass('col-sm-9');
+		iToggle.attr('data-toggle', 'true');
+	} else {
+		divProm.removeClass('col-sm-9');
+		divProm.addClass('col-sm-12');
+		iToggle.attr('data-toggle', 'false');
+	}
+});*/
 
