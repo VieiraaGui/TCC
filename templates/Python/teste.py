22cf14da2cf08c5bdb9a7b4525b064d1f04f04ec
Guilherme Vieira <guilhermevieiracosta25@gmail.com>
2019-10-30 20:37:44 -0300

    Implementando o Flask

---
+++ templates/Python/teste.py
@@ -0,0 +1,24 @@
+mport mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='35.198.38.24',
+                                         database='teste123',
+                                         user='root',
+                                         password='')
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Conectou foda no google ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("Your connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
